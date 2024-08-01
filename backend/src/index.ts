331eb3dbf35798ce59078016f50ad1cf3f5818b0
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-08-01 23:01:20 +0300

    Moved README and created REST API with NodeJs and Express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,14 @@
-console.log(
-  "This is a starter kit for this amazing project. With 💓 By Indian Coders"
-);
+import express from 'express';
+
+const app = express();
+
+// Register a new route
+
+app.use(express.json());
+
+app.delete("/user/:userid", (req, res, next) => {
+  console.log(req.params.userid);
+  return res.send("Hello");
+});
+
+app.listen(5000, () => console.log('Server is running on port 5000.')); // Server is running on port 5000.
