32579dcc9fdca6e6b40f51eda45866f1cd7852b1
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-08-02 00:02:25 +0300

    Set up MongoDB Database Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
-import express from 'express';
-
-const app = express();
-
-// Middlewares
-app.use(express.json());
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
 
 // Connections and listeners
-app.listen(5000, () => console.log('Server is running on port 5000.')); 
+connectToDatabase().then(() => {
+    app.listen(5000, () => console.log('Server is running on port 5000.')); 
+}).catch((err) => console.log(err));
