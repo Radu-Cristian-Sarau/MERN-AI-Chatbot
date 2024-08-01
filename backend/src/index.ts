6474931717d869cff078d8c801c5423b8fdb29c4
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-08-01 23:26:23 +0300

    Removed .env file from git

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,8 @@ import express from 'express';
 
 const app = express();
 
-// Register a new route
-
+// Middlewares
 app.use(express.json());
 
-app.delete("/user/:userid", (req, res, next) => {
-  console.log(req.params.userid);
-  return res.send("Hello");
-});
-
-app.listen(5000, () => console.log('Server is running on port 5000.')); // Server is running on port 5000.
+// Connections and listeners
+app.listen(5000, () => console.log('Server is running on port 5000.')); 
