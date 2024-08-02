6c458f7ca40a5970c651a823c8f2ae3a18b3b108
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-08-02 21:12:58 +0300

    Added routes
    with react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 const theme = createTheme({typography: { fontFamily: "Roboto Slab, sans-serif", allVariants: { color: "white" } }});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme = {theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme = {theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
