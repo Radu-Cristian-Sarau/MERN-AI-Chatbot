d46941b09d763dd4fb61f0073ebb1e0e2e5c6f0a
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-08-17 19:41:14 +0300

    Update OpenAI model
    from 3.5 turbo to 4o mini

--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -22,7 +22,7 @@ export const generateChatCompletion = async (req: Request, res: Response, next: 
         const openai = new OpenAIApi(config);
 
         // get latest response 
-        const chatResponse = await openai.createChatCompletion({ model: "gpt-3.5-turbo", messages: chats });
+        const chatResponse = await openai.createChatCompletion({ model: "gpt-4o-mini", messages: chats });
         user.chats.push(chatResponse.data.choices[0].message);
         await user.save();
         return res.status(200).json({ chats: user.chats });    
